03faada4b1a8d69612b88f09af66bda146602e80
martinez-cortes-macario <m23796376@gmail.com>
2026-03-02 19:10:05 -0800

    se agrego objetos de accion

---
+++ OOP/Ogro.py
@@ -0,0 +1,16 @@
+from enemigo import *
+import random 
+
+class Ogro(Enemigo):
+    def __init__(self, puntos_energia=28, ataque=3):
+        super().__init__(tipo_enemigo='Ogro', puntos_energia=puntos_energia, ataque=ataque)
+
+    def habla(self):
+        print("Ogro aplastar todo!!!")
+
+    def ataque_especial(self):
+        print("Ogro taque especial!!")
+        funcion_ataque_especial = random.random()<0.50
+        if funcion_ataque_especial:
+            self.puntos_energia += 4
+            print("Ogro enojado y incremento su ataque por 4!")
--- OOP/Zombie.py
+++ OOP/Zombie.py
@@ -1,11 +1,20 @@
-from Enemigo import *
+from enemigo import *
+import random
+class Zombie(Enemigo):
+    def __init__(self, puntos_energia=10, ataque=1):
+        super().__init__(Tipos_Enemigos='Zombie', puntos_energia=puntos_energia, ataque=ataque)
 
-class Zombie(enemigo):
-    def_init__(self, puntos_energia=10, ataque=1):
-    super().__init__(tipo_enemigo='Zombie', puntos_energia=puntos_energia, ataque=)
+    def habla(self):
+        print("*Hummmmmm...*")
 
-    def habla(self):}
-    print("*Hummmmm......*")
+    def propagar_enfermedad(self):
+        print("El Zombie esta tratando de propagar la enfermedad!!")
 
-    def propagar_enfermadad (self):
-        print("El Xombie esta tratando de propagar la enfermedad!!!")
+    def ataque_especial(self):
+        print("Zombie taque especial!!")
+        funcion_ataque_especial = random.random()<0.50
+        if funcion_ataque_especial:
+            self.puntos_energia += 2
+            print("Zombie ha regenerado su energia con 2HP!")
+
+
